f6c2a5fa5dfaa43ce4341c4d86a304a95bb94c4d
Szymon Chyc <erressrsch@gmail.com>
2024-02-02 15:30:17 +0100

    modifying temperature ranges

--- mod2_les7_task9/Program.cs
+++ mod2_les7_task9/Program.cs
@@ -12,22 +12,22 @@
             {
                 Console.WriteLine("Damn it's cold today");
             }
-            else if (temp >= 0 && temp <= 10)
+            else if (temp > 0 && temp <= 10)
             {
                 Console.WriteLine("It's cold");
             }
 
-            else if (temp >= 10 && temp <= 20)
+            else if (temp > 10 && temp <= 20)
             {
                 Console.WriteLine("It's coldly");
             }
 
-            else if (temp >= 20 && temp <= 30)
+            else if (temp > 20 && temp <= 30)
             {
                 Console.WriteLine("Temperature just right ");
             }
 
-            else if (temp >= 30 && temp <= 40)
+            else if (temp > 30 && temp <= 40)
             {
                 Console.WriteLine("It's starting be poorly becasue it's hot");
             }
